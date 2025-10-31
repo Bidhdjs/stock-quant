5e083decb56aa081c4cd18094f893e04a611b8c6
romanzhao <romanzhao@tencent.com>
2025-10-31 12:37:03 +0800

    rafactor

--- settings.py
+++ settings.py
@@ -19,7 +19,6 @@ html_root = project_root / 'html'
 
 
 # 交易策略相关参数
-PRINT_LOG = True    #是否打印日志
 MIN_ORDER_SIZE = 100    # 交易股票最小单位（股）
 MAX_PORTFOLIO_PERCENT = 0.8 # 最大持仓比例 = 总持仓股票数量 * 持仓股票价格 / 总资产
 MAX_SINGLE_BUY_PERCENT = 0.2    # 单笔交易百分比（买） = 单笔交易费用（ 单笔交易股票价格 * 单笔交易量） / 总资产
